70364753e1f275f280473c8dbf23197bb23c93e0
Nicola <nmihai@uw.edu>
2022-03-15 12:18:03 -0700

    c

--- list_registrations.php
+++ list_registrations.php
@@ -102,7 +102,7 @@ require_once 'config.inc.php';
         echo "</table>";
 
         echo "<div id=\"text-input\">";
-        echo '<a href="show_vehicle.php?id='  . $VIN . '">';
+        echo '<a href="show_vehicle.php?id='  . $VIN . '">Filter by VIN</a>';
         echo "</div>";
 
         echo "</div>";
