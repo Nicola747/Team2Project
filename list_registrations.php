f1e8c73024d68bb7bac6a2181fc40524f92f9956
Nicola <nmihai@uw.edu>
2022-03-15 12:11:49 -0700

    b

--- list_registrations.php
+++ list_registrations.php
@@ -102,7 +102,7 @@ require_once 'config.inc.php';
         echo "</table>";
 
         echo "<div id=\"text-input\">";
-        echo "<button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\" onclick=\"filter_registrations.php?id=\">Filter by VIN</button>";
+        echo "<a href=\"filter_registrations.php?id=\" class=\"btn btn-primary\" target=\"_blank\" rel=\noopener noreferrer\">Filter by VIN</a>";
         echo "</div>";
 
         echo "</div>";
