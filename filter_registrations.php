3f085ff39d9d5f21105ae936c088fed8eceb1a8b
Nicola <nmihai@uw.edu>
2022-03-15 12:42:48 -0700

    e

--- filter_registrations.php
+++ filter_registrations.php
@@ -5,51 +5,20 @@
 require_once 'config.inc.php';
 
 ?>
+
 <html>
 <head>
-    <title>Filter by VIN</title>
-    <meta name="description" content="Vehicle Registration Database">
-    <meta name="author" content="Nicola Mihai">
-    <link rel="icon" href="media/images/team2icon_v4.png">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index.css?v=1.0">
-    <link rel="stylesheet" href="css/registration.css?v=1.0">
-    <link rel="stylesheet" href="css/divStyle.css?v=1.0">
-
+    <title>Sample PHP Database Program</title>
+    <link rel="stylesheet" href="base.css">
 </head>
 <body>
+<?php
 
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="index.php">Team2 | Vehicle Registration Database</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item ">
-              <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-            </li>
-            <!-- <li class="nav-item ">
-              <a class="nav-link" href="list_vehicles.php">List All Vehicles</a>
-            </li> -->
-          </ul>
-          <a class="navbar-brand" id="group-names" href="">Aylin Onalan, Nicola Mihai, Kyle Kawahara, David Galenko</a>
-          
-          <!-- <form class="form-inline mt-2 mt-md-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form> -->
-        </div>
-      </nav>
-
+?>
 <div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h2 id="table-title">Full Registration List</h2>
+    <h2>Filter by VIN</h2>
     <?php
+
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database, $port);
 
@@ -58,8 +27,37 @@ require_once 'config.inc.php';
         die("Connection failed: " . $conn->connect_error);
     }
 
-	// Prepare SQL Statement
-    $sql = "SELECT year,make,model,VIN FROM Vehicle ORDER BY year";
+	// Check the Request is an Update from User -- Submitted via Form
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $VIN = $_POST['VIN'];
+	//echo $_POST['subject']; 
+        if ($VIN === null)
+            echo "<div><i>Specify a new name</i></div>";
+        else if ($VIN === false)
+            echo "<div><i>Specify a new name</i></div>";
+        else if (trim($VIN) === "")
+            echo "<div><i>Specify a new name</i></div>";
+        else {
+			
+            /* perform update using safe parameterized sql */
+            $sql = "SELECT * FROM Vehicle WHERE VIN LIKE '%?%'";
+            $stmt = $conn->stmt_init();
+            if (!$stmt->prepare($sql)) {
+                echo "failed to prepare";
+            } else {
+				
+				// Bind user input to statement
+                $stmt->bind_param('ss', $VIN,$id);
+				
+				// Execute statement and commit transaction
+                $stmt->execute();
+                $conn->commit();
+            }
+        }
+    }
+
+    /* Refresh the Data */
+    $sql = "SELECT * FROM Vehicle WHERE VIN LIKE '%?%'";
     $stmt = $conn->stmt_init();
     if (!$stmt->prepare($sql)) {
         echo "failed to prepare";
@@ -109,11 +107,16 @@ require_once 'config.inc.php';
 
         
     }
+    ?><br><br>
+            New Name: <input type="text" name="VIN">
+            <button type="submit">Update</button>
+        </form>
+    <?php
+    
 
-	// Close Connection
     $conn->close();
 
     ?>
-</div>
+</>
 </body>
 </html>
