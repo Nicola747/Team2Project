d86f64c8dbe8a5e377477112e76dd3fc3e93823a
Nicola <nmihai@uw.edu>
2022-03-15 13:18:04 -0700

    ?

--- filter_registrations.php
+++ filter_registrations.php
@@ -105,7 +105,7 @@ require_once 'config.inc.php';
     }
 
     /* Refresh the Data */
-    $sql = "SELECT year,make,model,VIN FROM Vehicle WHERE VIN LIKE '%$VIN_num%'";
+    $sql = "SELECT year,make,model,VIN FROM Vehicle WHERE VIN LIKE '%?%'";
     echo "a";
     echo $VIN_num;
     $stmt = $conn->stmt_init();
