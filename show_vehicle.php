3fbbcf0f33920acc0ad39006c4b49fafbe233529
Nicola <nmihai@uw.edu>
2022-03-14 23:55:07 -0700

    changes to modal

--- show_vehicle.php
+++ show_vehicle.php
@@ -181,14 +181,16 @@ if ($id === null) {
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <h5 class="modal-title" id="exampleModalLabel">Owner Information</h5>
                                 <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div class="modal-body">
-
-                                ...
+                            <div>
+                            
+                            </div>
+                                
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
