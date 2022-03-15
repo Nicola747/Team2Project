5d869a6271dbbc797d1baa25a64a516b4e121424
Nicola <nmihai@uw.edu>
2022-03-15 15:36:42 -0700

    q

--- show_vehicle.php
+++ show_vehicle.php
@@ -8,19 +8,19 @@
  */
 require_once 'config.inc.php';
 // Get Customer Number
-// $id = $_GET['id'];
-// if ($id === "") {
-//     header('location: list_customers.php');
-//     exit();
-// }
-// if ($id === false) {
-//     header('location: list_customers.php');
-//     exit();
-// }
-// if ($id === null) {
-//     header('location: list_customers.php');
-//     exit();
-// }
+$id = $_GET['id'];
+if ($id === "") {
+    header('location: list_customers.php');
+    exit();
+}
+if ($id === false) {
+    header('location: list_customers.php');
+    exit();
+}
+if ($id === null) {
+    header('location: list_customers.php');
+    exit();
+}
 ?>
 <html lang="en">
 
